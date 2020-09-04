f74ff8dabaa8c8094083e8cf10833030db2fea67
Fede <sergio.amoruso@southworks.com>
2020-09-04 00:13:13 -0300

    [backend] - add - search query handler

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const config = require('./services/config');
+const searchQueryHandler = require('./handlers/searchQueryHandler');
 
 const PORT = config.serverPort;
 
 const app = express();
+
+app.get('/api/items', searchQueryHandler);
 
 app.listen(PORT, () => console.log(`Server up and listening on port ${PORT}`));
