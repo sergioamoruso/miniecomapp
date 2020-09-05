a146bb50ad60b89c27722ec08a65b3f97f86fa4f
Fede <sergio.amoruso@southworks.com>
2020-09-05 13:06:28 -0300

    [backend] - add - processItems function

---
+++ backend/src/services/processItems.js
@@ -0,0 +1,18 @@
+const getDecimals = require('./getDecimals');
+
+module.exports = (items) => {
+  const processedItems = items.map((item) => ({
+    id: item.id,
+    title: item.title,
+    price: {
+      currency: item.currency_id,
+      amount: item.price,
+      decimals: getDecimals(item.price),
+    },
+    picture: item.thumbnail,
+    condition: item.condition,
+    free_shipping: item.shipping.free_shipping,
+  }));
+
+  return processedItems;
+};
---
+++ backend/src/services/processItems.test.js
@@ -0,0 +1,511 @@
+const items = [
+  {
+    id: 'MLA876762332',
+    site_id: 'MLA',
+    title: 'Moto G8 Dual Sim 64 Gb Blanco Prisma 4 Gb Ram',
+    seller: {
+      id: 606577097,
+      permalink: 'http://perfil.mercadolibre.com.ar/CELULARARG',
+      registration_date: '2020-07-09T12:23:54.000-04:00',
+      car_dealer: false,
+      real_estate_agency: false,
+      tags: [
+        'normal',
+        'user_info_verified',
+        'messages_as_seller',
+        'messages_as_buyer',
+      ],
+      seller_reputation: {
+        transactions: {
+          total: 53,
+          canceled: 5,
+          period: 'historic',
+          ratings: {
+            negative: 0.34,
+            positive: 0.66,
+            neutral: 0,
+          },
+          completed: 48,
+        },
+        power_seller_status: null,
+        metrics: {
+          claims: {
+            rate: 0,
+            value: 0,
+            period: '60 months',
+          },
+          delayed_handling_time: {
+            rate: 0,
+            value: 0,
+            period: '60 months',
+          },
+          sales: {
+            period: '60 months',
+            completed: 48,
+          },
+          cancellations: {
+            rate: 0,
+            value: 0,
+            period: '60 months',
+          },
+        },
+        level_id: '5_green',
+      },
+    },
+    price: 35490,
+    currency_id: 'ARS',
+    available_quantity: 1,
+    sold_quantity: 0,
+    buying_mode: 'buy_it_now',
+    listing_type_id: 'gold_special',
+    stop_time: '2040-08-30T04:00:00.000Z',
+    condition: 'new',
+    permalink:
+      'https://www.mercadolibre.com.ar/moto-g8-dual-sim-64-gb-blanco-prisma-4-gb-ram/p/MLA15566745',
+    thumbnail: 'http://http2.mlstatic.com/D_698020-MLA41797493867_052020-I.jpg',
+    accepts_mercadopago: true,
+    installments: {
+      quantity: 12,
+      amount: 5032.78,
+      rate: 70.17,
+      currency_id: 'ARS',
+    },
+    address: {
+      state_id: 'AR-B',
+      state_name: 'Buenos Aires',
+      city_id: null,
+      city_name: 'Munro',
+    },
+    shipping: {
+      free_shipping: true,
+      mode: 'me2',
+      tags: ['self_service_out', 'mandatory_free_shipping'],
+      logistic_type: 'drop_off',
+      store_pick_up: false,
+    },
+    seller_address: {
+      id: '',
+      comment: '',
+      address_line: '',
+      zip_code: '',
+      country: {
+        id: 'AR',
+        name: 'Argentina',
+      },
+      state: {
+        id: 'AR-B',
+        name: 'Buenos Aires',
+      },
+      city: {
+        id: null,
+        name: 'Munro',
+      },
+      latitude: '',
+      longitude: '',
+    },
+    attributes: [
+      {
+        name: 'Marca',
+        value_id: '2503',
+        value_struct: null,
+        attribute_group_name: 'Otros',
+        source: 1,
+        id: 'BRAND',
+        value_name: 'Motorola',
+        values: [
+          {
+            id: '2503',
+            name: 'Motorola',
+            struct: null,
+            source: 1,
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+      },
+      {
+        value_id: '7671213',
+        values: [
+          {
+            id: '7671213',
+            name: 'Adreno 610',
+            struct: null,
+            source: 1,
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+        source: 1,
+        id: 'GPU_MODEL',
+        name: 'Modelo de GPU',
+        value_name: 'Adreno 610',
+        value_struct: null,
+        attribute_group_name: 'Otros',
+      },
+      {
+        value_name: 'Nuevo',
+        values: [
+          {
+            id: '2230284',
+            name: 'Nuevo',
+            struct: null,
+            source: 2860837171021627,
+          },
+        ],
+        attribute_group_name: 'Otros',
+        source: 2860837171021627,
+        name: 'Condición del ítem',
+        value_id: '2230284',
+        value_struct: null,
+        attribute_group_id: 'OTHERS',
+        id: 'ITEM_CONDITION',
+      },
+      {
+        id: 'LINE',
+        name: 'Línea',
+        value_name: 'Moto G',
+        attribute_group_id: 'OTHERS',
+        attribute_group_name: 'Otros',
+        source: 1,
+        value_id: '3073863',
+        value_struct: null,
+        values: [
+          {
+            id: '3073863',
+            name: 'Moto G',
+            struct: null,
+            source: 1,
+          },
+        ],
+      },
+      {
+        value_id: '58944',
+        value_name: 'G8',
+        value_struct: null,
+        values: [
+          {
+            name: 'G8',
+            struct: null,
+            source: 1,
+            id: '58944',
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+        attribute_group_name: 'Otros',
+        id: 'MODEL',
+        name: 'Modelo',
+        source: 1,
+      },
+      {
+        value_struct: null,
+        attribute_group_name: 'Otros',
+        source: 1,
+        value_id: '7671215',
+        name: 'Modelo del procesador',
+        value_name: 'Snapdragon 665',
+        values: [
+          {
+            source: 1,
+            id: '7671215',
+            name: 'Snapdragon 665',
+            struct: null,
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+        id: 'PROCESSOR_MODEL',
+      },
+      {
+        source: 1,
+        id: 'WEIGHT',
+        value_id: '8172668',
+        values: [
+          {
+            id: '8172668',
+            name: '188.3 g',
+            struct: {
+              number: 188.3,
+              unit: 'g',
+            },
+            source: 1,
+          },
+        ],
+        attribute_group_name: 'Otros',
+        name: 'Peso',
+        value_name: '188.3 g',
+        value_struct: {
+          number: 188.3,
+          unit: 'g',
+        },
+        attribute_group_id: 'OTHERS',
+      },
+    ],
+    original_price: null,
+    category_id: 'MLA1055',
+    official_store_id: null,
+    domain_id: 'MLA-CELLPHONES',
+    catalog_product_id: 'MLA15566745',
+    tags: [
+      'brand_verified',
+      'extended_warranty_eligible',
+      'good_quality_picture',
+      'good_quality_thumbnail',
+      'loyalty_discount_eligible',
+      'immediate_payment',
+      'cart_eligible',
+      'shipping_guaranteed',
+    ],
+    catalog_listing: true,
+  },
+  {
+    id: 'MLA871241800',
+    site_id: 'MLA',
+    title: 'Moto G8 Power 64 Gb Smoke Black 4 Gb Ram',
+    seller: {
+      id: 71229887,
+      permalink: null,
+      registration_date: null,
+      car_dealer: false,
+      real_estate_agency: false,
+      tags: null,
+    },
+    price: 33699,
+    currency_id: 'ARS',
+    available_quantity: 1,
+    sold_quantity: 50,
+    buying_mode: 'buy_it_now',
+    listing_type_id: 'gold_special',
+    stop_time: '2040-07-27T04:00:00.000Z',
+    condition: 'new',
+    permalink:
+      'https://www.mercadolibre.com.ar/moto-g8-power-64-gb-smoke-black-4-gb-ram/p/MLA15579262',
+    thumbnail: 'http://http2.mlstatic.com/D_884450-MLA41632030529_052020-I.jpg',
+    accepts_mercadopago: true,
+    installments: {
+      quantity: 12,
+      amount: 4778.8,
+      rate: 70.17,
+      currency_id: 'ARS',
+    },
+    address: {
+      state_id: 'AR-C',
+      state_name: 'Capital Federal',
+      city_id: null,
+      city_name: 'Villa Crespo',
+    },
+    shipping: {
+      free_shipping: true,
+      mode: 'me2',
+      tags: ['self_service_in', 'mandatory_free_shipping'],
+      logistic_type: 'cross_docking',
+      store_pick_up: false,
+    },
+    seller_address: {
+      id: '',
+      comment: '',
+      address_line: '',
+      zip_code: '',
+      country: {
+        id: 'AR',
+        name: 'Argentina',
+      },
+      state: {
+        id: 'AR-C',
+        name: 'Capital Federal',
+      },
+      city: {
+        id: null,
+        name: 'Villa Crespo',
+      },
+      latitude: '',
+      longitude: '',
+    },
+    attributes: [
+      {
+        value_name: 'Motorola',
+        values: [
+          {
+            id: '2503',
+            name: 'Motorola',
+            struct: null,
+            source: 1,
+          },
+        ],
+        attribute_group_name: 'Otros',
+        id: 'BRAND',
+        value_id: '2503',
+        attribute_group_id: 'OTHERS',
+        source: 1,
+        name: 'Marca',
+        value_struct: null,
+      },
+      {
+        value_struct: null,
+        values: [
+          {
+            id: '7671213',
+            name: 'Adreno 610',
+            struct: null,
+            source: 1,
+          },
+        ],
+        attribute_group_name: 'Otros',
+        source: 1,
+        id: 'GPU_MODEL',
+        name: 'Modelo de GPU',
+        value_id: '7671213',
+        value_name: 'Adreno 610',
+        attribute_group_id: 'OTHERS',
+      },
+      {
+        values: [
+          {
+            id: '2230284',
+            name: 'Nuevo',
+            struct: null,
+            source: 6808261514773724,
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+        id: 'ITEM_CONDITION',
+        name: 'Condición del ítem',
+        value_id: '2230284',
+        value_struct: null,
+        value_name: 'Nuevo',
+        attribute_group_name: 'Otros',
+        source: 6808261514773724,
+      },
+      {
+        name: 'Línea',
+        value_id: '3073863',
+        value_name: 'Moto G',
+        value_struct: null,
+        values: [
+          {
+            id: '3073863',
+            name: 'Moto G',
+            struct: null,
+            source: 1,
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+        attribute_group_name: 'Otros',
+        id: 'LINE',
+        source: 1,
+      },
+      {
+        values: [
+          {
+            name: 'G8 Power',
+            struct: null,
+            source: 1,
+            id: '8202803',
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+        id: 'MODEL',
+        value_id: '8202803',
+        value_name: 'G8 Power',
+        value_struct: null,
+        attribute_group_name: 'Otros',
+        source: 1,
+        name: 'Modelo',
+      },
+      {
+        value_struct: null,
+        values: [
+          {
+            name: 'Snapdragon 665',
+            struct: null,
+            source: 1,
+            id: '7671215',
+          },
+        ],
+        attribute_group_id: 'OTHERS',
+        attribute_group_name: 'Otros',
+        source: 1,
+        id: 'PROCESSOR_MODEL',
+        name: 'Modelo del procesador',
+        value_id: '7671215',
+        value_name: 'Snapdragon 665',
+      },
+      {
+        id: 'WEIGHT',
+        value_id: '1095137',
+        value_name: '195 g',
+        attribute_group_id: 'OTHERS',
+        attribute_group_name: 'Otros',
+        name: 'Peso',
+        value_struct: {
+          number: 195,
+          unit: 'g',
+        },
+        values: [
+          {
+            id: '1095137',
+            name: '195 g',
+            struct: {
+              number: 195,
+              unit: 'g',
+            },
+            source: 1,
+          },
+        ],
+        source: 1,
+      },
+    ],
+    original_price: null,
+    category_id: 'MLA1055',
+    official_store_id: null,
+    domain_id: 'MLA-CELLPHONES',
+    catalog_product_id: 'MLA15579262',
+    tags: [
+      'good_quality_thumbnail',
+      'loyalty_discount_eligible',
+      'brand_verified',
+      'extended_warranty_eligible',
+      'good_quality_picture',
+      'immediate_payment',
+      'cart_eligible',
+    ],
+    catalog_listing: true,
+  },
+];
+
+describe('processItems should', () => {
+  beforeEach(() => {
+    sut = require('./processItems');
+  });
+
+  test('process items correctly', () => {
+    const result = sut(items);
+
+    const expected = [
+      {
+        id: 'MLA876762332',
+        title: 'Moto G8 Dual Sim 64 Gb Blanco Prisma 4 Gb Ram',
+        price: {
+          currency: 'ARS',
+          amount: 35490,
+          decimals: null,
+        },
+        picture:
+          'http://http2.mlstatic.com/D_698020-MLA41797493867_052020-I.jpg',
+        condition: 'new',
+        free_shipping: true,
+      },
+      {
+        id: 'MLA871241800',
+        title: 'Moto G8 Power 64 Gb Smoke Black 4 Gb Ram',
+        price: {
+          currency: 'ARS',
+          amount: 33699,
+          decimals: null,
+        },
+        picture:
+          'http://http2.mlstatic.com/D_884450-MLA41632030529_052020-I.jpg',
+        condition: 'new',
+        free_shipping: true,
+      },
+    ];
+
+    expect(result).toEqual(expected);
+  });
+});
