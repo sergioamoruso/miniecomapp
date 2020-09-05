bb332517555143a0d82ffa7e899865adc47aaa8f
Fede <sergio.amoruso@southworks.com>
2020-09-05 16:26:22 -0300

    [frontend] - add - Search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Search from './components/search/Search';
+
 function App() {
   return (
-    <div className="App">
-    <h1>E-commerce App</h1>
+    <div className="app">
+      <Search />
     </div>
   );
 }
