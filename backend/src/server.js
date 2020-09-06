919c4af2b5b3713f504664b07afe970dc0800976
Fede <sergio.amoruso@southworks.com>
2020-09-06 17:54:48 -0300

    [backend] - update - server.js to include search item endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const config = require('./services/config');
 const searchQueryHandler = require('./handlers/searchQueryHandler');
+const searchItemHandler = require('./handlers/searchItemHandler');
 
 const PORT = config.serverPort;
 
 const app = express();
 
 app.get('/api/items', searchQueryHandler);
+
+app.get('/api/items/:id', searchItemHandler);
 
 app.listen(PORT, () => console.log(`Server up and listening on port ${PORT}`));
