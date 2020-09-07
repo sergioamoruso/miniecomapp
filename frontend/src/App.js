9420cf8721098fac9e72ae36d84911df4af2d72c
Fede <sergio.amoruso@southworks.com>
2020-09-07 20:17:16 -0300

    [frontend] - add - search request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Search from "./components/search/Search";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const executeSearchQueryRequest = async (query) => {
+    const requestUrl = `/api/items?q=${query}`;
+    console.log({ msg: "Executing request...", value: requestUrl });
+    const response = await axios.get(requestUrl);
+    console.log({ msg: "Request successful", value: response.data.items });
+    setItems(response.data.items);
+  };
+
   return (
     <Router>
       <div className="app">
         <Route exact path="/">
-          <Search />
+          <Search executeSearchQueryRequest={executeSearchQueryRequest} />
         </Route>
       </div>
     </Router>
