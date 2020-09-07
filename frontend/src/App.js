bd7607eff157aca21b9f5718f68ff7097d44e828
Fede <sergio.amoruso@southworks.com>
2020-09-07 09:41:44 -0300

    [frontend] - add - support for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
 
-import Search from './components/search/Search';
+import Search from "./components/search/Search";
 
 function App() {
   return (
-    <div className="app">
-      <Search />
-    </div>
+    <Router>
+      <div className="app">
+        <Route exact path="/">
+          <Search />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
