76db831b1dbe384b6501f985e034c674811f1800
Fede <sergio.amoruso@southworks.com>
2020-09-07 20:17:16 -0300

    [frontend] - add - search request

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -2,23 +2,23 @@ import React, { useState } from "react";
 import "./Search.css";
 import logo from "../../assets/logo.png";
 
-function Search() {
+function Search({ executeSearchQueryRequest }) {
   const [searchText, setSearchText] = useState("");
 
-  const submitSearch = (e) => {
+  const submitSearch = async (e) => {
     e.preventDefault();
-    console.log(searchText);
+    await executeSearchQueryRequest(searchText);
   };
 
   return (
     <form onSubmit={submitSearch} className="container">
-      <img src={logo} className="logo" alt="Mercado Libre Logo"/>
-        <input
-          type="text"
-          placeholder="Nunca dejes de buscar"
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <input type="submit" value="" />
+      <img src={logo} className="logo" alt="Mercado Libre Logo" />
+      <input
+        type="text"
+        placeholder="Nunca dejes de buscar"
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <input type="submit" value="" />
     </form>
   );
 }
