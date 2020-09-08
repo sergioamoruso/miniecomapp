361354aae3ef823b824624a24abcd8c1c144983f
Fede <sergio.amoruso@southworks.com>
2020-09-08 15:37:05 -0300

    [frontend] - update - Search component to remove extra functionality

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,23 +1,16 @@
 import React, { useState } from "react";
 import "./Search.css";
 import logo from "../../assets/logo.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-function Search({ executeSearchQueryRequest }) {
-  const [searchText, setSearchText] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+function Search({ setSearchText, submitSearch }) {
 
-  const submitSearch = async (e) => {
-    setIsLoading(true);
+  const onSubmit = async (e) => {
     e.preventDefault();
-    await executeSearchQueryRequest(searchText);
-    await timeout(2000); // Timeout for UX purposes only (let the user know that the search is being processed)
-    setIsLoading(false);
-  };
+    await submitSearch();
+  }
 
   return (
-    <form onSubmit={submitSearch} className="container">
+    <form onSubmit={onSubmit} className="container">
       <img src={logo} className="logo" alt="Mercado Libre Logo" />
       <input
         type="text"
@@ -25,15 +18,8 @@ function Search({ executeSearchQueryRequest }) {
         onChange={(e) => setSearchText(e.target.value)}
       />
       <input type="submit" value="" />
-      {isLoading && (
-        <FontAwesomeIcon className="spinner" icon={faSpinner} spin size="3x" />
-      )}
     </form>
   );
-}
-
-function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export default Search;
