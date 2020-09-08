6e1f4152a2cdc0fc0395c010d1c390bb24c46001
Fede <sergio.amoruso@southworks.com>
2020-09-07 21:13:32 -0300

    [frontend] - add - spinner in Search page

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import "./Search.css";
 import logo from "../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function Search({ executeSearchQueryRequest }) {
   const [searchText, setSearchText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const submitSearch = async (e) => {
+    setIsLoading(true);
     e.preventDefault();
     await executeSearchQueryRequest(searchText);
+    await timeout(2000); // Timeout for UX purposes only (let the user know that the search is being processed)
+    setIsLoading(false);
   };
 
   return (
@@ -19,8 +25,15 @@ function Search({ executeSearchQueryRequest }) {
         onChange={(e) => setSearchText(e.target.value)}
       />
       <input type="submit" value="" />
+      {isLoading && (
+        <FontAwesomeIcon className="spinner" icon={faSpinner} spin size="3x" />
+      )}
     </form>
   );
+}
+
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export default Search;
