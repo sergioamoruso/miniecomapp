b80409efeec23af4fa65717e3a5cc061f3e490c7
Fede <sergio.amoruso@southworks.com>
2020-09-10 19:52:01 -0300

    [frontend] - update - move Search component to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,71 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import axios from "axios";
 
-import Main from "./components/main/Main";
+import Search from "./components/search/Search";
 import Results from "./components/results/Results";
 import Detail from "./components/detail/Detail";
 
 import "./App.css";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [categories, setCategories] = useState([]);
-
-  const executeSearchQueryRequest = async (query) => {
-    const requestUrl = `/api/items?q=${query}`;
-    console.log({ msg: "Executing request...", value: requestUrl });
-    const response = await axios.get(requestUrl);
-    console.log({
-      msg: "Request successful",
-      categories: response.data.categories,
-      items: response.data.items,
-    });
-    setCategories(response.data.categories);
-    setItems(response.data.items);
-  };
-
   return (
     <Router>
       <div className="app">
+        <Search />
         <Switch>
-          <Route exact path="/">
-            <Main executeSearchQueryRequest={executeSearchQueryRequest} />
-          </Route>
+          <Route exact path="/" render={() => <div></div>} />
 
-          <Route
-            exact
-            path="/items"
-            render={(props) => {
-              const params = new URLSearchParams(props.location.search);
-              const query = params.get("search");
-              console.log({
-                msg: "Received GET /items?search=",
-                value: query,
-              });
+          <Route exact path="/items" render={() => <Results />} />
 
-              return (
-                <Results
-                  history={props.history}
-                  executeSearchQueryRequest={executeSearchQueryRequest}
-                  query={query}
-                  categories={categories}
-                  items={items}
-                />
-              );
-            }}
-          />
-
-          <Route
-            exact
-            path="/items/:id"
-            render={() => (
-              <Detail
-                executeSearchQueryRequest={executeSearchQueryRequest}
-                categories={categories}
-              />
-            )}
-          />
+          <Route exact path="/items/:id" render={() => <Detail />} />
         </Switch>
       </div>
     </Router>
