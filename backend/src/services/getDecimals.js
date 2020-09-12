78fdaec6426e4fa1096727901767e17880ca0349
Fede <sergio.amoruso@southworks.com>
2020-09-12 14:06:53 -0300

    [backend] - update - getDecimals function to return values with more precision

--- backend/src/services/getDecimals.js
+++ backend/src/services/getDecimals.js
@@ -1,9 +1,6 @@
 module.exports = (number) => {
   // Convert to string to avoid dealing with floating point precision
-  const converted = number.toString();
-
-  // If it does not contain decimals, return null
-  if (!converted.includes('.')) return null;
+  const converted = number.toFixed(2);
 
   const decimals = converted.split('.')[1];
 
