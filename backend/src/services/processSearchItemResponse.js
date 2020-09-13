8e89ac33f83642a568092ca670b73338d2df6787
Fede <sergio.amoruso@southworks.com>
2020-09-13 13:52:43 -0300

    [backend] - add - item category request

--- backend/src/services/processSearchItemResponse.js
+++ backend/src/services/processSearchItemResponse.js
@@ -1,9 +1,9 @@
 const processSearchItemResult = require('./processSearchItemResult');
 const signResponse = require('./signResponse');
 
-module.exports = (data, description) => {
+module.exports = (data, description, category) => {
   const response = {
-    item: processSearchItemResult(data, description),
+    item: processSearchItemResult(data, description, category),
   };
 
   const signedResponse = signResponse(response);
--- backend/src/services/processSearchItemResult.js
+++ backend/src/services/processSearchItemResult.js
@@ -1,6 +1,6 @@
 const getDecimals = require('./getDecimals');
 
-module.exports = (item, description) => ({
+module.exports = (item, description, category) => ({
   id: item.id,
   title: item.title,
   price: {
@@ -13,4 +13,5 @@ module.exports = (item, description) => ({
   free_shipping: item.shipping.free_shipping,
   sold_quantity: item.sold_quantity,
   description: description.plain_text,
+  categories: category.path_from_root.map((category) => category.name),
 });
